1f8c7566de6aaf1b9fd6090aef768c718798a710
yegri <exsilver305@gamil.com>
2022-09-27 23:33:09 +0900

    좌표를 이용한 007 성능개선

--- main.js
+++ main.js
@@ -3,16 +3,21 @@ const horizontal = document.querySelector('.horizontal');
 const target = document.querySelector('.target');
 const tag = document.querySelector('.tag');
 
-document.addEventListener('mousemove', (event) => {
-    const x = event.clientX;
-    const y = event.clientY;
-    console.log(`${x} ${y}`);
 
-    vertical.style.left = `${x}px`;
-    horizontal.style.top = `${y}px`;
-    target.style.left = `${x}px`;
-    target.style.top = `${y}px`;
-    tag.style.left = `${x}px`;
-    tag.style.top = `${y}px`;
-    tag.innerHTML = `${x}px ${y}px`;
+addEventListener('load', () => {
+
+    const targetRect = target.getBoundingClientRect();
+    const targetHalfWidth = targetRect.width / 2;
+    const targetHalfHeight = targetRect.height / 2;
+
+    document.addEventListener('mousemove', (event) => {
+        const x = event.clientX;
+        const y = event.clientY;
+
+        vertical.style.transform = `translateX(${x}px)`;
+        horizontal.style.transform = `translateY(${y}px)`;
+        target.style.transform = `translate(${x-targetHalfWidth}px, ${y-targetHalfHeight}px)`;
+        tag.style.transform = `translate(${x}px, ${y}px)`;
+        tag.innerHTML = `${x}px ${y}px`;
+    });
 });
